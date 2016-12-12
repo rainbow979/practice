d2a039c30e583f4d0337f3c1624ee0475a504f19
hzwer <598460606@163.com>
2016-12-13 00:30:50 +0800

    Used requests

--- spider/hzwer.py
+++ spider/hzwer.py
@@ -1,32 +1,26 @@
 # 统计博客hzwer.com上的代码行数
+# -*- coding: utf-8 -*-
 
 __author__ = 'hzwer'
-# -*- coding: utf-8 -*-  
 
 import re
-from urllib.request import *
+import requests
 
 home = 'http://hzwer.com/'
 tot = 0
 page = 0
-
-for i in range(1, 8000):
-    try:
-        url = home + str(i) + '.html'
-        request = Request(url)
-        response = urlopen(request)
-        content = response.read().decode("UTF-8").replace("\r\n", "\n")
-    except:
-        pass
-    else:
-        pattern = re.compile('-webkit-tab-size:4; tab-size:4; font-size: 15px !important; line-height: 16px !important;">\n(.*?)</textarea>', re.S)
-        code = re.findall(pattern, content)
-        if(code):
-            length = 0
-            for j in code:
-                length = length + len(j.splitlines())
+ 
+for i in range(1, 10000):
+    url = home + str(i) + '.html' #生成url
+    r = requests.get(url)
+    content = r.text
+    pattern = re.compile('-webkit-tab-size:4; tab-size:4; font-size: 15px !important; line-height: 16px !important;">\n(.*?)</textarea>', re.S) #正则匹配
+    code = re.findall(pattern, content)
+    if(code):
+        length = 0
+        for j in code:
+            length = length + len(j.splitlines())
             tot += length
             print('url: {} {}行代码，合计{}行代码'.format(url, length, tot))
-            page = page + 1
-
-print('共{}个页面，{}行代码'.format(page, tot))
+            page = page + 1  
+print('共{}个页面，{}行代码'.format(page, tot))                
